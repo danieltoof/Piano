e5a84818f73cf6a8cf1c094d4a4331eaba1b6318
MorrisKoning <s1172354@windesheim.nl>
2024-04-23 09:56:06 +0200

    omschreven naar GetLength(); functie RoundedTimeSpan verwijderd

--- midi-test/Program.cs
+++ midi-test/Program.cs
@@ -1,39 +1,60 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using CleanCode.Data;
 using Melanchall.DryWetMidi.Core;
 using Melanchall.DryWetMidi.Interaction;
 using Melanchall.DryWetMidi.Multimedia;
-using System.Numerics;
+
+static void GetLength(string MidiFileName)
+{
+    // check of bestandsnaam opgegeven is
+    if (!MidiFileName.EndsWith(".mid"))
+    {
+        //voeg bestandsnaam toe aan string
+        MidiFileName = MidiFileName + ".mid";
+    }
+
+    try
+    {
+        // inladen midi
+
+        var midi = MidiFile.Read(@"..\..\..\midis\" + MidiFileName);
+
+        // lees lengte van midi
+        TimeSpan MidiFileDuration = midi.GetDuration<MetricTimeSpan>();
+
+        // lengte timespan omzetten naar minuten en secondes
+        int minutes = (Convert.ToInt32(MidiFileDuration.TotalSeconds) / 60);
+        int seconds = (Convert.ToInt32(MidiFileDuration.TotalSeconds) % 60);
+
+        string secondsString;
+
+        // 0 voor de secondes plakken als ze onder 10 zijn
+        if (seconds < 10)
+        {
+            secondsString = "0" + seconds;
+        }
+        else
+        {
+            secondsString = seconds.ToString();
+        }
+
+        // minuten en secondes aan elkaar plakken
+        String midiLengte = minutes.ToString() + ":" + secondsString;
+
+        // print final waarde
+        Console.WriteLine(midiLengte);
+    } catch (FileNotFoundException)
+    {
+        Console.WriteLine("ERROR: MIDI file niet gevonden.");
+    }
+    
+}
+
+string a = Console.ReadLine();
+GetLength(a);
 
 var midi = MidiFile.Read(@"..\..\..\midis\Darude_-_Sandstorm__Izzet_Selanik__Arne_Mulder_20061031124814.mid");
 //var midi = MidiFile.Read(@"..\..\..\midis\AUD_HTX0525.mid");
-
-// lees en print lengte van midi
-TimeSpan midiFileDuration = midi.GetDuration<MetricTimeSpan>();
-Console.WriteLine(midiFileDuration);
-
-Console.WriteLine(midiFileDuration.TotalSeconds);
-Console.WriteLine(Convert.ToInt32(midiFileDuration.TotalSeconds));
-
-int minutes = (Convert.ToInt32(midiFileDuration.TotalSeconds)/60);
-int seconds = (Convert.ToInt32(midiFileDuration.TotalSeconds)%60);
-
-Console.WriteLine(minutes+ ":" + seconds); //print lengte todo: zorg voor nullen onder 10 en niet hoger dan 59
-
-string secondsString;
-
-if (seconds < 10)
-{
-    secondsString = "0" + seconds;
-} else
-{
-    secondsString = seconds.ToString();
-}
-
-Console.WriteLine(secondsString);
-String midiLengte = minutes.ToString() + ":" + secondsString;
-Console.WriteLine(midiLengte);
 
 using (var output = OutputDevice.GetByName("Microsoft GS Wavetable Synth"))
 using (var playback = midi.GetPlayback(output))
