849fc1c6dd0d95c054241eb93dbced068eec9864
MorrisKoning <s1172354@windesheim.nl>
2024-04-24 13:32:43 +0200

    get tempo functies

--- midi-test/Program.cs
+++ midi-test/Program.cs
@@ -4,7 +4,7 @@ using Melanchall.DryWetMidi.Core;
 using Melanchall.DryWetMidi.Interaction;
 using Melanchall.DryWetMidi.Multimedia;
 
-static void GetLength(string MidiFileName)
+static String GetLength(string MidiFileName)
 {
     // check of bestandsnaam opgegeven is
     if (!MidiFileName.EndsWith(".mid"))
@@ -13,42 +13,86 @@ static void GetLength(string MidiFileName)
         MidiFileName = MidiFileName + ".mid";
     }
 
-    try
+    // inladen midi
+    var midi = MidiFile.Read(MidiFileName);
+
+    // lees lengte van midi
+    TimeSpan MidiFileDuration = midi.GetDuration<MetricTimeSpan>();
+
+    // lengte timespan omzetten naar minuten en secondes
+    int minutes = (Convert.ToInt32(MidiFileDuration.TotalSeconds) / 60);
+    int seconds = (Convert.ToInt32(MidiFileDuration.TotalSeconds) % 60);
+
+    string secondsString;
+
+    // 0 voor de secondes plakken als ze onder 10 zijn
+    if (seconds < 10)
     {
-        // inladen midi
-        var midi = MidiFile.Read(MidiFileName);
+        secondsString = "0" + seconds;
+    }
+    else
+    {
+        secondsString = seconds.ToString();
+    }
 
-        // lees lengte van midi
-        TimeSpan MidiFileDuration = midi.GetDuration<MetricTimeSpan>();
+    // minuten en secondes aan elkaar plakken
+    String midiLengte = minutes.ToString() + ":" + secondsString;
 
-        // lengte timespan omzetten naar minuten en secondes
-        int minutes = (Convert.ToInt32(MidiFileDuration.TotalSeconds) / 60);
-        int seconds = (Convert.ToInt32(MidiFileDuration.TotalSeconds) % 60);
+    // print final waarde
+    //Console.WriteLine(midiLengte);
 
-        string secondsString;
+    return midiLengte;
+}
 
-        // 0 voor de secondes plakken als ze onder 10 zijn
-        if (seconds < 10)
+static int GetStartTempo(string MidiFileName)
+{
+    // inladen midi
+    var midi = MidiFile.Read(MidiFileName);
+
+    // haal tempo van midi op
+    using (var tempoMapManager = new TempoMapManager(
+        midi.TimeDivision,
+        midi.GetTrackChunks().Select(c => c.Events)))
+    {
+        // maken tempomap
+        TempoMap tempoMap = tempoMapManager.TempoMap;
+
+        // lezen start tempo
+        int tempoStart = (int)Math.Round(tempoMap.GetTempoAtTime((MidiTimeSpan)1).BeatsPerMinute);
+
+        return tempoStart;
+    }
+}
+static int GetHighestTempo(string MidiFileName)
+{
+    // inladen midi
+    var midi = MidiFile.Read(MidiFileName);
+
+    // haal tempo van midi op
+    using (var tempoMapManager = new TempoMapManager(
+        midi.TimeDivision,
+        midi.GetTrackChunks().Select(c => c.Events)))
+    {
+        // maken tempomap
+        TempoMap tempoMap = tempoMapManager.TempoMap;
+
+        // checken hoogste tempo in midi
+        var tempoList = tempoMap.GetTempoChanges().Select(valueChange => valueChange.Value).ToList();
+        var orderedBPMList = tempoList.Select(tempo => tempo.BeatsPerMinute).Order();
+        // check voor als er geen tempo changes zijn - midi default dan naar 120 BPM
+        if (orderedBPMList.Count() == 0)
         {
-            secondsString = "0" + seconds;
+            //Console.WriteLine("120 BPM");
+            int returnTempo = 120;
+            return returnTempo;
         }
         else
         {
-            secondsString = seconds.ToString();
+            //Console.WriteLine(orderedBPMList.Last() + " BPM");
+            return (int)Math.Round(orderedBPMList.Last());
         }
-
-        // minuten en secondes aan elkaar plakken
-        String midiLengte = minutes.ToString() + ":" + secondsString;
-
-        // print final waarde
-        Console.WriteLine(midiLengte);
-
-    } 
-    catch (FileNotFoundException)
-    {
-        Console.WriteLine("ERROR: MIDI file niet gevonden.");
     }
-    
+
 }
 
 //string a = Console.ReadLine();
@@ -69,16 +113,28 @@ while (true)
     //Console.WriteLine(randomsong);
 
     var midi = MidiFile.Read(randomsong);
-    GetLength(randomsong);
+    Console.WriteLine(GetLength(randomsong));
+
+    if (GetStartTempo(randomsong) == GetHighestTempo(randomsong))
+    {
+        Console.WriteLine(GetStartTempo(randomsong)+" BPM");
+    }
+    else
+    {
+        Console.WriteLine("Start tempo: " + GetStartTempo(randomsong) + " BPM");
+        Console.WriteLine("Highest tempo: " + GetHighestTempo(randomsong) + " BPM");
+    }
+
 
 
 
     using (var output = OutputDevice.GetByName("Microsoft GS Wavetable Synth"))
     using (var playback = midi.GetPlayback(output))
     {
+                
         playback.Speed = 1.0;
         //playback.Play();
-        midi.Play(output);
+        midi.Play(output); //uitgecomment zodat ie m niet eerst hoeft af te spelen
         //Console.WriteLine("playing midi");
         System.Threading.Thread.Sleep(2000);
         Console.WriteLine();
