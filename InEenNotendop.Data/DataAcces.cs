7b762e428176bd8e0c649adb5af76fa9bcae1154
whateveryeti26 <95095152+whateveryeti26@users.noreply.github.com>
2024-05-28 11:20:46 +0200

    ssh tunnel werkt en connectie naar database nu ook laatste querys moeten nog aangepast worden.

--- InEenNotendop.Data/DataAcces.cs
+++ InEenNotendop.Data/DataAcces.cs
@@ -1,126 +1,45 @@
 ﻿using Microsoft.Data.SqlClient;
+using System.Diagnostics;
+using Renci.SshNet;
 
 namespace InEenNotendop.Data
 {
     public class DataProgram
     {
-        public string ConnectionString { get; private set; }
+        public string ConnectionString = "Data Source=127.0.0.1,1433;Initial Catalog=PianoHeroDB;User ID=Newlogin;Password=VeryStr0ngP@ssw0rd;Encrypt=False;";
 
-        public void StartDataBase()
+        public void StartSshTunnel()
         {
-            try
+            ProcessStartInfo psi = new ProcessStartInfo
             {
-                string DBname = "PianoHeroDB";
-                string user = System.Net.Dns.GetHostName() + "\\" + Environment.UserName;
-
-                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-                builder.DataSource = "(localdb)\\MSSQLLocalDB";
-                builder.IntegratedSecurity = true;
-                builder.UserID = user;
-                builder.Password = "";
-                builder.ApplicationIntent = ApplicationIntent.ReadWrite;
-
-                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
-                {
-                    connection.Open();
-
-                    // Check if the database exists
-                    string checkDatabaseExistsQuery = $"SELECT 1 FROM sys.databases WHERE name = '{DBname}'";
-                    using (SqlCommand checkDatabaseExistsCommand = new SqlCommand(checkDatabaseExistsQuery, connection))
-                    {
-                        object result = checkDatabaseExistsCommand.ExecuteScalar();
-                        if (result == null)
-                        {
-                            // Database does not exist, create it
-                            string createDatabaseQuery = $"CREATE DATABASE {DBname}";
-                            using (SqlCommand createDatabaseCommand = new SqlCommand(createDatabaseQuery, connection))
-                            {
-                                createDatabaseCommand.ExecuteNonQuery();
-                            }
-
-                            // Switch to the newly created database
-                            builder.InitialCatalog = DBname;
-                            connection.ChangeDatabase(DBname);
-
-                            // Create tables
-                            string createTablesQuery = @"USE PianoHeroDB
-                                DROP TABLE IF EXISTS Scores;
-                                DROP TABLE IF EXISTS Nummers;
-
-                                CREATE TABLE Nummers (
-                                    Title VARCHAR(255),
-                                    Artiest VARCHAR(255),
-                                    Lengte INT,
-                                    Bpm INT,
-                                    Moeilijkheid INT NOT NULL,
-                                    ID INT IDENTITY(1,1) PRIMARY KEY,
-                                    Filepath varchar(255),
-                                    CONSTRAINT CHK_Moeilijkheidsgraad CHECK (Moeilijkheid >= 1 AND Moeilijkheid <= 3)
-                                    );
-
-                                CREATE TABLE Scores (
-                                    Score INT,
-                                    NummerID INT,
-                                    FOREIGN KEY (NummerID) REFERENCES Nummers(ID)
-                                    );
-
-
-                                DECLARE @CurrentTitle VARCHAR(255) = 'Title1';
-                                DECLARE @CurrentTitleNumber INT = 1;
-
-                                DECLARE @CurrentArtist VARCHAR(255) = 'Artiest1';;
-                                DECLARE @CurrentArtistNumber INT = 1;
-
-
-
-                                WHILE @CurrentTitleNumber <= 25
-                                BEGIN
-                                    SET @CurrentTitle = CONCAT('Title', @CurrentTitleNumber);
-                                    SET @CurrentArtist = CONCAT('Artist', @CurrentArtistNumber)
-
-                                    DECLARE @RandomLengte INT = ABS(CHECKSUM(NEWID())) % 151 + 150;
-                                    DECLARE @RandomBpm INT = ABS(CHECKSUM(NEWID())) % 101 + 80;
-                                    DECLARE @RandomMoeilijkheid INT = ABS(CHECKSUM(NEWID())) % 3 + 1;
-
-                                    INSERT INTO Nummers (Title, Artiest, Lengte, Bpm, Moeilijkheid)
-                                    VALUES (@CurrentTitle, @CurrentArtist, @RandomLengte, @RandomBpm, @RandomMoeilijkheid);
-
-                                    DECLARE @LastInsertedId INT;
-                                    SET @LastInsertedId = SCOPE_IDENTITY();
-
-                                    INSERT INTO Scores (Score, NummerID)
-                                    VALUES (0, @LastInsertedId);
-
-                                    SET @CurrentTitleNumber = @CurrentTitleNumber + 1;
-                                    SET @CurrentArtistNumber = @CurrentArtistNumber + 1;
-                                END";
-
-                            using (SqlCommand createTablesCommand = new SqlCommand(createTablesQuery, connection))
-                            {
-                                createTablesCommand.ExecuteNonQuery();
-                            }
-                        }
-                        else
-                        {
-                            // Database exists, switch to it
-                            builder.InitialCatalog = DBname;
-                            connection.ChangeDatabase(DBname);
-                        }
-                    }
-                    connection.Close();
-                }
-
-                // Set the connection string property
-                ConnectionString = builder.ConnectionString;
-            }
-            catch (Exception ex)
-            {
-                // Handle exceptions as needed
-                Console.WriteLine(ex.Message);
-            }
-            
+                FileName = "powershell.exe",
+                Arguments = ".\\sshtunnel.ps1",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true
+            };
         }
 
+        public void DownloadSong() 
+        {
+            string host = "145.44.235.225";
+            string username = "student";
+            string password = "PianoHero";
+            string remoteFilePath = $"/home/student/Music/{/*filename*/}";
+            string localSavePath = $".\\Resources\\Songs\\{/*file.mid*/}";
+            using (var sftp = new SftpClient(host, username, password))
+            {
+                sftp.Connect();
+
+                using (Stream fileStream = File.Create(localSavePath))
+                {
+                    sftp.DownloadFile(remoteFilePath, fileStream);
+                }
+
+                sftp.Disconnect();
+            }
+        }
         public int getNummersAmount()
         {
             if (!string.IsNullOrEmpty(ConnectionString))
@@ -150,7 +69,7 @@ namespace InEenNotendop.Data
             {
                 using(var command = connection.CreateCommand())
                 {
-                    command.CommandText = "SELECT n.Title, n.Artiest, n.Lengte, n.Bpm, n.Moeilijkheid, n.ID, n.Filepath, s.Score FROM dbo.Nummers AS n INNER JOIN dbo.Scores AS s ON n.ID = s.NummerID";
+                    command.CommandText = "SELECT Title, Artiest, Lengte, Bpm, Moeilijkheid, ID, Filepath, Score FROM Nummers INNER JOIN Scores ON Nummers.ID = Scores.NummerID;";
                     connection.Open();
                     using(var reader = command.ExecuteReader())
                     {
@@ -173,7 +92,7 @@ namespace InEenNotendop.Data
                                 filepath = null;
                             }
                             int score = reader.GetInt32(reader.GetOrdinal("Score"));
-
+                          
                             Nummer nummer = new Nummer(title,artiest,lengte,bpm,moeilijkheid,id,filepath,score);
 
                             nummers.Add(nummer);
@@ -197,11 +116,11 @@ namespace InEenNotendop.Data
                 {
                     if (Difficulty != 0)
                     {
-                        command.CommandText = $"SELECT n.Title, n.Artiest, n.Lengte, n.Bpm, n.Moeilijkheid, n.ID, n.Filepath, s.Score FROM dbo.Nummers AS n INNER JOIN dbo.Scores AS s ON n.ID = s.NummerID WHERE Moeilijkheid = {Difficulty} ORDER BY {Sort}";
+                        command.CommandText = $"SELECT Title, Artiest, Lengte, Bpm, Moeilijkheid, ID, Filepath, Score FROM Nummers INNER JOIN Scores ON Nummers.ID = Scores.NummerID WHERE Moeilijkheid = {Difficulty} ORDER BY {Sort}";
                     }
                     else
                     {
-                        command.CommandText = $"SELECT n.Title, n.Artiest, n.Lengte, n.Bpm, n.Moeilijkheid, n.ID, n.Filepath, s.Score FROM dbo.Nummers AS n INNER JOIN dbo.Scores AS s ON n.ID = s.NummerID ORDER BY {Sort}";
+                        command.CommandText = $"SELECT Title, Artiest, Lengte, Bpm, Moeilijkheid, ID, Filepath, Score FROM Nummers INNER JOIN Scores ON Nummers.ID = Scores.NummerID ORDER BY {Sort}";
                     }
                     
                     connection.Open();
@@ -246,7 +165,7 @@ namespace InEenNotendop.Data
             {
                 using (var command = connection.CreateCommand())
                 {
-                    command.CommandText = $"SELECT n.Title, n.Artiest, n.Lengte, n.Bpm, n.Moeilijkheid, n.ID, n.Filepath, s.Score FROM dbo.Nummers AS n INNER JOIN dbo.Scores AS s ON n.ID = s.NummerID WHERE Moeilijkheid = {Difficulty}";
+                    command.CommandText = $"SELECT Title, Artiest, Lengte, Bpm, Moeilijkheid, ID, Filepath, Score FROM Nummers INNER JOIN Scores ON Nummers.ID = Scores.NummerID WHERE Moeilijkheid = {Difficulty}";
                     connection.Open();
                     using (var reader = command.ExecuteReader())
                     {
