6782a33ac55eaf4f928305058fc0290f29dfcab8
ThisIsNotVi <s1180794@student.windesheim.nl>
2024-06-17 10:24:15 +0200

    Update MidiInputprocessor.cs

--- InEenNotendop.Business/MidiInputprocessor.cs
+++ InEenNotendop.Business/MidiInputprocessor.cs
@@ -86,17 +86,3 @@ namespace InEenNotendop.Business
         }
     }
 }
-
-
-#region ThisRegionCanBeIgnored
-public class Program
-{
-    public static void Main()
-    {
-        // NIET VERWIJDEREN, HET MOET EEN MAIN HEBBEN
-        // WAAROM? VRAAG NIET AAN MIJ
-        // NEGEER DIT DUS
-    }
-}
-
-#endregion
