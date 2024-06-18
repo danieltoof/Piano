3031e6e9235d27acfdd7cff0fa055a226f7889be
ThisIsNotVi <s1180794@student.windesheim.nl>
2024-06-18 10:17:21 +0200

    interfaces gemaakt

---
+++ InEenNotendop.Data/IMidiInterface.cs
@@ -0,0 +1,7 @@
+﻿namespace InEenNotendop.Data;
+
+public interface IMidiInterface
+{
+    void UploadSongToServer(string name, string artist, string localPath){}
+    void DownloadSong(string artist, string title){}
+}
