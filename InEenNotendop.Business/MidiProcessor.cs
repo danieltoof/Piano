2e8f2e563304548cb56b9e05138d4b1259f501d3
Daniel van der Velde <daniel@vandervelde.info>
2024-06-26 12:13:41 +0200

    Fixed bug

--- InEenNotendop.Business/MidiProcessor.cs
+++ InEenNotendop.Business/MidiProcessor.cs
@@ -74,19 +74,29 @@ namespace InEenNotendop.Business
 
         public void Dispose()
         {
-            try {
+            try
+            {
+                MidiDeviceNotFound = null;
+
                 MidiPlayer?.Dispose();
                 Stopwatch?.Stop();
                 Stopwatch?.Reset();
-            } catch (Exception e) 
+
+                if (_midiIn != null)
+                {
+                    _midiIn.Stop();
+                    _midiIn.Dispose();
+                    _midiIn = null;
+                }
+            }
+            catch (Exception e)
             {
                 Debug.WriteLine($"Error during disposal: {e.Message}");
             }
-            
         }
 
-        //Voor event wanneer nummer klaar is
-        public void OnSongFinished()
+            //Voor event wanneer nummer klaar is
+            public void OnSongFinished()
         {
             Score = ScoreCalculator.CalculateScore(SongForNotePlayback, _songPlayed);
             this.Dispose();
